eecf2e0a7294cc08976895e9aecb9a05353a58af
lizhenxiu <875337431@qq.com>
2018-03-14 18:53:39 +0800

    tab跳转

--- README.md
+++ README.md
@@ -7,7 +7,39 @@ npm i react-router-dom -S
 `Switch 只渲染出第一个与当前访问地址匹配的 <Route> 或 <Redirect>。渲染出第一个匹配的路由`
 
 
-###语法
+### 语法
 1.PureComponent/Component，对于拥有内部state，使用生命周期的函数的组件，我们可以使用二者之一，但是大部分情况下，我更推荐使用PureComponent，因为它提供了更好的性能，同时强制你使用不可变的对象，保持良好的编程习惯。
 
 PureComponent：改变了生命周期方法shouldComponentUpdate，并且它会自动检查组件是否需要重新渲染。这时，只有PureComponent检测到state或者props发生变化时，PureComponent才会调用render方法
+
+
+#### 生命周期
+实例化
+首次实例化
+
+getDefaultProps
+getInitialState
+componentWillMount
+render
+componentDidMount
+实例化完成后的更新
+
+getInitialState
+componentWillMount
+render
+componentDidMount
+存在期
+组件已存在时的状态改变
+
+componentWillReceiveProps
+shouldComponentUpdate
+componentWillUpdate
+render
+componentDidUpdate
+销毁&清理期
+componentWillUnmount
+
+
+
+#### match对象 
+match是一个匹配路径参数的对象，它有一个属性params，里面的内容就是路径参数，除常用的params属性外，它还有url、path、isExact属性
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,56 +1,138 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios';
+import moment from 'moment'
+import 'moment/locale/zh-cn';
 import { Link } from 'react-router-dom';
 
 
-const Home = () => (
-    <div>
-        <div className="h-panel">
-            <div className="h-header">
-                <a className="ui-tab">全部</a>
-                <a className="ui-tab on">精华</a>
-                <a className="ui-tab">分享</a>
-                <a className="ui-tab">问答</a>
-                <a className="ui-tab">招聘</a>
-                <a className="ui-tab">测试</a>
-            </div>
-            <div className="list-box">
-                <div className="cell">
-                    <a className="user-avatar pull-left">
-                        <img src="https://avatars2.githubusercontent.com/u/3140898?v=4&amp;s=120" alt="shanelau" />
-                    </a>
-                    <span className="reply-count pull-left"><span className="count-of-replies" title="回复数">5</span>
-                        <span className="count-sperator">/</span>
-                        <span className="count-of-visits" title="点击数">489</span>
-                    </span>
-                    <a className="last-time pull-right">
-                        <img className="user-small-avatar" src="https://avatars2.githubusercontent.com/u/3140898?v=4&amp;s=120" alt="shanelau" />
-                        <span className="last-active-time">7 分钟前</span>
-                    </a>
-                    <div className="topic-title-wrapper">
-                        <span className=" tag tab-common">招聘</span>
-                        <a className="topic-title" href="#/topic/5a8f806371327bb413bbfd8c">『杭州』星火矿池招聘「会计师」</a>
-                    </div>
-                </div>
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            postList: [],
+            tagType: '',
+        }
+    }
+    componentWillMount() {
+        const type = this.props.match.params.type
+        this.setState({
+            tagType: type || 'all'
+        })
+        this.getPostData(type).then(res => {
+            if (res.status === 200) {
+                this.setState({
+                    postList: res.data.data
+                });
+            } else {
+                console.error(res.statusText)
+            }
+        }).catch(e => {
+            console.warn(e);
+        });
 
+    }
+    componentDidMount() {
+           
+    }
+    componentWillReceiveProps() {
+        const type = this.props.match.params.type
+        this.setState({
+            tabTypes: type || 'all'
+        })
+        if (this.state.tagType !== type) {
+            this.getPostData(type).then(res => {
+                if (res.status === 2000) {
+                    this.setState({
+                        postList: res.data.data
+                    })
+                 
+                } else {
+                    console.error(res.statusText)
+                }
+            }).catch(e => {
+                console.warn(e)
+            })
+        }
+
+    }
+    getPostData(type) {
+        return axios.get('https://cnodejs.org/api/v1/topics',{
+                params: {
+                    tab: type,
+                }
+            })
+    }
+    tabTypes(post) {
+        const tab = post.tab
+        const map = {
+            'top': '置顶',
+            'share': '分享',
+            'good': '精华',
+            'ask': '问答',
+            'job': '招聘'
+        }
+        if (post.tab) {
+            return map['top']
+        }
+        else if (post.good) {
+            return map['good'];
+        } else {
+            return map[tab];
+        }
+    }
+    getRouterTab() {
+        const routeList = [
+            { type: 'all', path: '/', text: '全部' },
+            { type: 'elite', path: 'tag/elite', text: '精华' },
+            { type: 'share', path: 'tag/share', text: '分享' },
+            { type: 'ask', path: 'tag/ask', text: '问答' },
+            { type: 'jop', path: 'tag/jop', text: '招聘' },
+            { type: 'text', path: 'tag/text', text: '测试' }
+        ]
+        return routeList.map((router) => (
+            <Link to={router.path} path={router.path} className={this.state.tagType === router.type ? "ui-tab on" : "ui-tab"}>{router.text}</Link>
+        ))
+    }
+    render() {
+        // console.log(this.state.postList)
+        const listHtml = () => {
+            return this.state.postList.map((post, index) => (
                 <div className="cell">
                     <a className="user-avatar pull-left">
-                        <img src="https://avatars2.githubusercontent.com/u/3140898?v=4&amp;s=120" alt="shanelau" />
+                        <img src={post.author.avatar_url}  alt="1" />
                     </a>
-                    <span className="reply-count pull-left"><span className="count-of-replies" title="回复数">5</span>
+                    <span className="reply-count pull-left"><span className="count-of-replies" title="回复数">{post.reply_count}</span>
                         <span className="count-sperator">/</span>
-                        <span className="count-of-visits" title="点击数">489</span>
+                        <span className="count-of-visits" title="点击数">{post.visit_count}</span>
                     </span>
                     <a className="last-time pull-right">
-                        <img className="user-small-avatar" src="https://avatars2.githubusercontent.com/u/3140898?v=4&amp;s=120" alt="shanelau" />
-                        <span className="last-active-time">7 分钟前</span>
+                        <img className="user-small-avatar" src={post.author.avatar_url} alt={post.author.loginname}  />
+                        <span className="last-active-time">{moment(post.last_reply_at).fromNow()}</span>
                     </a>
                     <div className="topic-title-wrapper">
-                        <span className=" tag tab-common">招聘</span>
-                        <a className="topic-title" href="#/topic/5a8f806371327bb413bbfd8c">『杭州』星火矿池招聘「会计师」</a>
+                        <span className={(post.top || post.good) ? 'tag put-top': ' tag tab-common' }>{this.tabTypes(post)}</span>
+                        <Link className="topic-title"  to={'/topic/'+post.id}>{post.title}</Link>
+                    </div>
+                </div>
+            ))
+        }
+        const nodataHtml = () => (
+            <div className="no-data">
+                <h5>暂无数据...</h5>
+            </div>
+        )
+        
+
+        return (
+            <div>
+                <div className="h-panel">
+                    <div className="h-header">{this.getRouterTab()}</div>
+                    <div className="list-box">
+                        {this.state.postList.length ? listHtml() : nodataHtml()}
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-)
+        )
+    }
+}
 export default Home
